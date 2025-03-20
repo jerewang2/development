035dc8fbb2128185d8db7b65bc1ae6edfabf32bf
jerewang2 <jerewang1@gmail.com>
2025-03-19 23:38:32 -0400

    Dealer mechanic working.

--- Assignments/Blackjack.py
+++ Assignments/Blackjack.py
@@ -12,7 +12,7 @@ warnings.filterwarnings("ignore")
 class Blackjack:
     def __init__(self):
         self.deck = {}
-        self.ranks = ['A', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+        self.ranks = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
         self.suits = ['\u2663', '\u2665', '\u2666', '\u2660']
         self.parameters = []
         self.card_totals = []
@@ -37,11 +37,18 @@ class Blackjack:
     
     def setup(self):
         for i in range(int(self.parameters[1]) + 1):
-            self.players[i + 1] = []
+            if i == int(self.parameters[1]):
+                self.players['Dealer'] = []
+            else:
+                self.players[i + 1] = []
         
         for i in range(2):
-            for j in range(int(self.parameters[1]) + 1):    
+            # Draw 1 card per player
+            for j in range(int(self.parameters[1])):    
                 self.players[j + 1].append(self.draw_card())
+            
+            # Draw 1 card for Dealer
+            self.players['Dealer'].append(self.draw_card())
 
     def retrieve_and_process(self):
         with open('Assignments/blackjack_parameters.txt', 'r') as text:
@@ -108,13 +115,12 @@ class Blackjack:
 
     def play_game(self):
         # Check if dealer wins
-        # if 
+        if self.calculate_hand_value(self.players['Dealer']) == 21:
+            print("Dealer has 21. Dealer wins!")
+            return
 
         for player, hand in self.players.items():
-            if int(player) == int(self.parameters[1]) + 1:
-                # This is the dealer
-                pass
-            else:
+            if player != 'Dealer':
                 while True:
                     print(f'Player {player} here is your hand: {hand}')
                     card_total = self.calculate_hand_value(hand)
@@ -129,20 +135,36 @@ class Blackjack:
                     if self.machine_learning(card_total)[0] == 'success':
                         print('AI suggests you can hit!')
                     else:
-                        print('AI suggests you stay.')
+                        print('AI suggests you stand.')
 
-                    decision = input("Would you like to hit or pass? ")
+                    decision = input("Would you like to hit or stand? ")
                     
                     if decision == 'hit':
                         new_card = self.draw_card()
                         print(f'You drew a {new_card}!')
                         hand.append(new_card)
-                    elif decision == 'pass':
+                    elif decision == 'stand':
                         print(f'Great, your turn is complete.')
                         print(f'Final hand: {hand}\n')
                         break
                     else:
                         print("Invalid input. Please try again.")
+            elif player == 'Dealer':
+                while True:
+                    print(f'Dealer hand: {hand}')
+                    dealer_card_total = self.calculate_hand_value(hand)
+                    if dealer_card_total > 21:
+                        print('Dealer busts! Players win!')
+                        break
+                    elif dealer_card_total < 16:
+                        dealer_new_card = self.draw_card()
+                        print(f'Dealer draws a {dealer_new_card}')
+                        hand.append(dealer_new_card)
+                    elif dealer_card_total >= 16 and dealer_card_total <= 21:
+                        print(f'Dealer stands.')
+                        print(f'Dealer hand: {hand}\n')
+                        break
+
 
     def run(self):
         # self.plot_raw_data()
